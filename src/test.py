abf1db118de2bcb400f040bd5668e2141e466ee2
hoyun06 <hoyun06@gmail.com>
2025-04-12 21:50:45 +0900

    refactor: 파일명 변경에 따른 import 수정

--- src/test.py
+++ src/test.py
@@ -2,8 +2,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from src.mylexer import *
-from src.myparser import *
+from src.tokenizer import *
+from src.parse import *
 
 pParse = Parse()
 set_parse_object(pParse)
