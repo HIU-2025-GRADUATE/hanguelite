3263052ecb7018a402c2d439d0731429202a4937
hoyun06 <hoyun06@gmail.com>
2025-05-09 15:55:00 +0900

    refactor: Expr 생성자 로직 오류 해결

--- src/sqliteInt.py
+++ src/sqliteInt.py
@@ -133,7 +133,7 @@ class Expr:
         self.op = op
         self.pLeft = pLeft
         self.pRight = pRight
-        self.token = token or Token()
+        self.token = token or Token("")
         self.pList = None
         self.iTable = 0
         self.iColumn = 0
