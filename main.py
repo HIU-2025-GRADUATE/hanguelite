57ed6dcef38741e1b7b27050b2c442e20e6b6c17
kckc0608 <kckc0608@naver.com>
2025-05-31 21:07:11 +0900

    chore: main.py 코드 정리

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import os
 
 def runParser(parse: Parse, sql: str):
     set_parse_object(parse)
-    result = parser.parse(sql, debug=False)
-    print(result)
+    parser.parse(sql, debug=False)
 
 def execute_sql(db, sql):
     parse = Parse(db)
@@ -14,28 +13,15 @@ def execute_sql(db, sql):
 
 
 def main():
-#     # Test it out
-#     data = '''CREATE TABLE test'''
-#
-#     # Give the lexer some input
-#     lexer.input(data)
-#
-#     # Tokenize
-#     while True:
-#         tok = lexer.token()
-#         if not tok:
-#             break  # No more input
-#         print(tok)
-#
     db = sqlite.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'db'))
     while True:
         try:
             s = input('sql > ')
         except EOFError:
             break
-        if not s: continue
 
-        execute_sql(db, s)
+        if s:
+            execute_sql(db, s)
 
 if __name__ == '__main__':
     main()
