5b7f08f08c4c928dfc6e9327f0d49f34f5900039
hoyun06 <hoyun06@gmail.com>
2025-06-26 15:41:50 +0900

    feat: where문 처리 시 필요한 토큰 정의

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -7,12 +7,18 @@ reserved = {
     'IN' : 'TK_IN',
     'CREATE' : 'TK_CREATE',
     'TABLE' : 'TK_TABLE',
+    'WHERE' : 'TK_WHERE',
+    'AND' : 'TK_AND',
+    'OR' : 'TK_OR',
+    'LIKE' : 'TK_LIKE'
 }
 
 # 토큰 이름 목록: parse.y에서 사용되는 토큰들과 SQLite의 tokenize.c에 있는 키워드들
 tokens = (
     'TK_STAR', 'TK_ID', 'TK_DOT', 'TK_COLUMN', 'TK_IGNORE',
-    'TK_STRING', 'TK_LP', 'TK_RP', 'TK_COMMA',
+    'TK_STRING', 'TK_LP', 'TK_RP', 'TK_COMMA', 'TK_INTEGER', 
+    'TK_FLOAT', 'TK_LT', 'TK_GT', 'TK_LE', 'TK_GE', 'TK_NE', 
+    'TK_EQ'
 ) + tuple(reserved.values())
 
 # 정규표현식 규칙
@@ -22,13 +28,29 @@ t_TK_IGNORE     = r' \t\n'
 t_TK_LP         = r'\('
 t_TK_RP         = r'\)'
 t_TK_COMMA      = r','
-
-t_ignore        = ' \t'
+t_TK_LE         = r'<='
+t_TK_GE         = r'>='
+t_TK_NE         = r'<>'
+t_TK_EQ         = r'='
+t_TK_LT         = r'<'
+t_TK_GT         = r'>'
 
 # 식별자 처리: 예약어와 일반 ID 구분
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     t.type = reserved.get(t.value.upper(), 'TK_ID')
+    return t
+
+def t_FLOAT(t):
+    r'[0-9]+\.[0-9]+'
+    t.value = float(t.value)
+    t.type = 'TK_FLOAT'
+    return t
+
+def t_INTEGER(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    t.type = 'TK_INTEGER'
     return t
 
 # def t_STRING(t):
