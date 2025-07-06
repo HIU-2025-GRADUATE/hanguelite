d41689de4e6bde062b61687f48bf072ae82b65d6
kckc0608 <kckc0608@naver.com>
2025-07-06 16:24:12 +0900

    chore: gdbm_store 디폴트 값 제거, 타입 명시

--- src/gdbm.py
+++ src/gdbm.py
@@ -4,18 +4,10 @@ from io import TextIOWrapper
 GDBM_REPLACE = 0
 GDBM_INSERT = 1
 
-def gdbm_store(dbf: TextIOWrapper = 0, key='', data='', mode=0):
-    # dbf.seek(0)
-    # db = {l[0]:l[1:] for l in csv.reader(dbf)}
-    # if mode == GDBM_INSERT and key in db:
-    #     return "ERROR"
-    # db[key]=data
-    dbf.write(str(key) + "," + ",".join(map(str, data)) + '\n')
+def gdbm_store(dbf: TextIOWrapper, key, data: list, mode=GDBM_REPLACE):
+    record = [key] + data
+    dbf.write(",".join(map(str, record)) + '\n')
     dbf.flush()
-    # with open(os.path.abspath(dbf.name), "a+", encoding='utf-8') as f:
-    #     for key in db:
-    #         f.write(str(key)+","+','.join(map(str, db[key]))+'\n')
-    # del db
 
 def gdbm_open(filePath, mode):
     return open(filePath, mode, encoding='utf-8')
