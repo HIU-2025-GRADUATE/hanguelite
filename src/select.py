270872a6a2dba9dc9cbb245cb1fbd9cd63e5fe11
hoyun06 <hoyun06@gmail.com>
2025-07-13 17:41:41 +0900

    chore: 불필요한 주석 제거

--- src/select.py
+++ src/select.py
@@ -11,18 +11,7 @@ def fillInColumnList(pParse : Parse, p : Select):
     if pTabList.a[i].pTab:
       return 0
     
-    # ================================= #
-    # select * from tableA 쿼리가 정상 동작하도록 하는 코드
-    # newTab = Table("tableA")
-    # newTab.nCol = 6
-    # newTab.aCol.append(Column("rowid"))
-    # newTab.aCol.append(Column("학번"))
-    # newTab.aCol.append(Column("이름"))
-    # newTab.aCol.append(Column("학년"))
-    # newTab.aCol.append(Column("전공"))
-    # newTab.aCol.append(Column("전화번호"))
-    # ================================= #
-    pTabList.a[i].pTab = findTable(pParse.db, pTabList.a[i].zName);  # build.c 파일에 구현된 함수
+    pTabList.a[i].pTab = findTable(pParse.db, pTabList.a[i].zName);  
     if pTabList.a[i].pTab == None: 
     #   sqliteSetString(&pParse.zErrMsg, "no such table: ", .a[i].zName, 0);
       pParse.nErr += 1
