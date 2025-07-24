3680a7ce340cae51288011f89757e2a3df100fb3
hoyun06 <hoyun06@gmail.com>
2025-07-24 14:57:08 +0900

    refactor: hashNoCase 함수 수정

--- src/util.py
+++ src/util.py
@@ -16,17 +16,12 @@ _state_machine = [
 ]
 
 def hashNoCase(z : str, n : int):
-    UpperToLower = [i for i in range(256)]
-    for i in range(ord('A'), ord('Z') + 1):
-        UpperToLower[i] = ord(chr(i).lower())
-
     h = 0
     if n <= 0:
         n = len(z)
 
     for i in range(n):
-        c = ord(z[i])
-        h = (h << 3) ^ h ^ UpperToLower[c]
+        h = (h << 3) ^ h ^ ord(z[i].lower())
 
     if h < 0:
         h = -h
