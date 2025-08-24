9ac639dd7e2c1542c12ad77cc6fea79fb7fe7d72
hoyun06 <hoyun06@gmail.com>
2025-08-24 15:35:14 +0900

    feat: 한글 문법 설계

--- src/parse.py
+++ src/parse.py
@@ -178,9 +178,10 @@ def p_select(p):
     p[0] = p[1]
 
 def p_oneselect(p):
-    """oneselect : TK_SELECT selcollist from where_opt groupby_opt having_opt orderby_opt"""
+    # """oneselect : TK_SELECT selcollist from where_opt groupby_opt having_opt orderby_opt"""
+    """oneselect : from where_opt groupby_opt having_opt selcollist TK_COL_LIST_PARTICLE_KR orderby_opt TK_SELECT_KR"""
     # Create a new SELECT structure using the parsed select list and from clause.
-    p[0] = Select(p[2], p[3], p[4], p[5], p[6], p[7], 0)
+    p[0] = Select(p[5], p[1], p[2], p[3], p[4], p[7], 0)
 
 def p_selcollist_star(p):
     """selcollist : TK_STAR"""
@@ -203,7 +204,7 @@ def p_sclp_empty(p):
     p[0] = None
 
 def p_from(p):
-    """from : TK_FROM seltablist"""
+    """from : seltablist TK_FROM_KR"""
     p[0] = p[2]
 
 def p_stl_prefix_empty(p):
@@ -238,7 +239,7 @@ def p_where_opt_empty(p):
     p[0] = None  
 
 def p_where_opt_expr(p):
-    """where_opt : TK_WHERE expr"""
+    """where_opt : expr TK_WHERE_KR"""
     p[0] = p[2]  
 
 def p_groupby_opt_empty(p):
@@ -246,7 +247,7 @@ def p_groupby_opt_empty(p):
     p[0] = None  
 
 def p_groupby_opt(p):
-    """groupby_opt : TK_GROUP TK_BY exprlist"""
+    """groupby_opt : exprlist TK_GROUP_BY_KR"""
     p[0] = p[3] 
 
 def p_having_opt_empty(p):
@@ -254,7 +255,7 @@ def p_having_opt_empty(p):
     p[0] = None  
 
 def p_having_opt(p):
-    """having_opt : TK_HAVING expr"""
+    """having_opt : TK_HAVING_KR expr TK_HAVING_PARTICLE_KR"""
     p[0] = p[2] 
 
 def p_orderby_opt_empty(p):
@@ -262,7 +263,7 @@ def p_orderby_opt_empty(p):
     p[0] = None  
 
 def p_orderby_opt(p):
-    """orderby_opt : TK_ORDER TK_BY sortlist"""
+    """orderby_opt : sortlist TK_ORDER_BY_KR"""
     p[0] = p[3] 
 
 def p_sortlist_comma(p):
@@ -283,11 +284,11 @@ def p_sortitem(p):
     p[0] = p[1]
 
 def p_sortorder_asc(p):
-    """sortorder : TK_ASC"""
+    """sortorder : TK_ASC_KR"""
     p[0] = 0
 
 def p_sortorder_desc(p):
-    """sortorder : TK_DESC"""
+    """sortorder : TK_DESC_KR"""
     p[0] = 1
 
 def p_sortorder_empty(p):
