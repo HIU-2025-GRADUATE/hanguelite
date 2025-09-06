0e28868c8e3ffb8ace388058a47b28f3ef301ff3
kckc0608 <kckc0608@naver.com>
2025-09-06 09:52:15 +0900

    feat: update 구문 문법 추가

--- src/parse.py
+++ src/parse.py
@@ -173,6 +173,16 @@ def p_command_update(p):
     update(pParse, table, setList, whereOpt)
     p[0] = p[1]
 
+def p_set_list(p):
+    """setlist : setlist TK_COMMA id TK_EQ expr"""
+    p[0] = p[1].append(p[5], p[3])
+
+def p_set_list_single(p):
+    """setlist : id TK_EQ expr"""
+    exprList = ExprList()
+    exprList.append(p[3], p[1])
+    p[0] = exprList
+
 """
     SELECT
 """
