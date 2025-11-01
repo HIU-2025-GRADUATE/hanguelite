2aa522f5c0c703a1a9a7bff530875194abea2278
Klare <34192865+freedomera@users.noreply.github.com>
2025-11-01 11:05:01 +0900

    fix: (vdbe) Sort 계열 Opcode 수정, src/sorter 추가 (#58)
    * fix: (vdbe) Sort 계열 Opcode 수정, src/sorter 추가
    
    * fix: (vdbe) continue 구문 앞에 pc+=1 삽입하여 무한 루프 수정
    
    * fix: (vdbe) continue 구문 대신 적절한 오류 코드 추가
    
    * fix: (vdbe) continue 구문 대신 적절한 오류 코드 추가
    
    * feat: (vdbe) OP_NextIdx, OP_PutIdx, OP_DeleteIdx 구현

--- src/vdbe/vdbe.py
+++ src/vdbe/vdbe.py
@@ -2,6 +2,7 @@ from src.dbbe import *
 from src.vdbe.vdbeOp import *
 from src.vdbe.cursor import *
 from src.vdbe.agg import *
+from src.vdbe.sorter import *
 from ..constant import SQLITE_INTERNAL, SQLITE_OK
 from ..exception.exception import BadInstruction
 from src.util import *
@@ -56,6 +57,7 @@ class Vdbe:
     self.apList = list()     # /* An open file for each list */
     # int nSort;         # /* Number of slots in apSort[] */
     # Sorter **apSort;   # /* An open sorter list */
+    self.apSort = list()
     # FILE *pFile;       # /* At most one open file handler */
     # int nField;        # /* Number of file fields */
     # char **azField;    # /* Data for each file field */
@@ -814,6 +816,7 @@ class Vdbe:
         elif pOp.opcode == OP_Put:
           print("cursor id:", pOp.p1)
           if pOp.p1 < 0 or pOp.p1 >= self.nCursor or self.aCsr[pOp.p1] == 0:
+            pc += 1
             continue
           data = self.aStack[-1]
           key = self.aStack[-2]
@@ -841,6 +844,7 @@ class Vdbe:
         # 만약 조회하는 커서의 KeyAsData 값이 1 이라면 데이터 대신 키 값을 읽어옴
         elif pOp.opcode == OP_Field:
           if pOp.p1 < 0 or pOp.p1 >= self.nCursor or self.aCsr[pOp.p1].pCursor == 0:
+            pc += 1
             continue
           if self.aCsr[pOp.p1].keyAsData:
             z = self.aCsr[pOp.p1].pCursor.readKey()
@@ -858,19 +862,18 @@ class Vdbe:
             self.aStack.append(z)
 
         elif pOp.opcode == OP_Rewind:
-          if pOp.p1 < 0 or pOp.p1 >= self.nCursor or self.aCsr[pOp.p1] == 0:
+          if pOp.p1<0 or pOp.p1>=self.nCursor or self.aCsr[pOp.p1]==0: 
+            pc += 1
             continue
           self.aCsr[pOp.p1].pCursor.rewind()
 
         # p1 커서의 dbf 에서 가리키고 있는 레코드의 다음 레코드를 가리키도록 이동
         elif pOp.opcode == OP_Next:
-          if pOp.p1 < 0 or pOp.p1 >= self.nCursor or self.aCsr[pOp.p1] == 0:
+          if pOp.p1<0 or pOp.p1>=self.nCursor or self.aCsr[pOp.p1]==0: 
+            pc += 1
             continue
-
-          if self.aCsr[pOp.p1].pCursor.nextKey() == 0:
-            pc = pOp.p2-1
-          else:
-            self.nFetch += 1
+          if self.aCsr[pOp.p1].pCursor.nextKey() == 0: pc = pOp.p2-1            #16 => makeLabel 없어서 하드 코딩
+          else: self.nFetch+=1
 
         # p1 커서의 다음 커서를 처음 커서 (0번) 으로 리셋
         elif pOp.opcode == OP_ResetIdx:
@@ -878,38 +881,61 @@ class Vdbe:
           if 0 <= i < self.nCursor:
             self.aCsr[i].index = 0
 
+        # ** P1 커서는 SQL 인덱스를 가리킵니다. 해당 커서로
+        # ** 가장 최근에 가져온 데이터는 여러 개의 정수들로 이루어져 있으며,
+        # ** 각 정수는 SQL 테이블 파일의 레코드 키입니다.
+        # ** 이 명령은 P1의 데이터에서 다음 정수 테이블 키를 가져와
+        # ** 스택에 푸시합니다. fetch 이후 이 명령이 처음 실행되면
+        # ** 첫 번째 정수 테이블 키가 푸시되고, 이후 실행될 때마다
+        # ** 다음 정수 테이블 키가 순차적으로 푸시됩니다.
+        # **
+        # ** 이 명령을 실행할 때 P1의 데이터에 더 이상 정수 테이블 키가
+        # ** 남아 있지 않다면, 아무 것도 푸시하지 않고 즉시 P2에 해당하는
+        # ** 명령으로 점프합니다.
+        
+        # TODO: 여기 로직 다 변경됐음. 확인 필요
         elif pOp.opcode == OP_NextIdx:
           i = pOp.p1
-          self.aStack.append(0)
-          if 0 <= i < self.nCursor and self.aCsr[i].pCursor is not None:
-            nIdx = self.aCsr[i].pCursor.dataLength()
-            aIdx = self.aCsr[i].pCursor.readData(0)
+          pCrsr = self.aCsr[i].pCursor
+          if i >= 0 and i < self.nCursor and pCrsr:
+            k = pCrsr.dataLength()
 
-            if nIdx > 1:
-              # TODO k = *(aIdx++)
-              k = aIdx[1]
-              if k > nIdx-1:
-                k = nIdx-1
-            else:
-              k = nIdx
-            
-            for j in range(self.aCsr[i].index, k):
-              if aIdx[j] != 0:
-                self.aStack[-1] = aIdx[j]
+            j = self.aCsr[i].index
+            while j < k:
+              if pCrsr.data[j] != 0:
+                self.aStack.append(pCrsr.data[j])
                 break
-
+              j += 1
+            
             if j >= k:
               j = -1
-              pc = pOp.p2-1
-              self.aStack.pop()
+              pc = pOp.p2 - 1
             
             self.aCsr[i].index = j+1
 
         # 스택의 top은 SQL index 키, 그 다음 값은 SQL table entry 키인 정수 값
         # tos의 인덱스 키와 일치하는 레코드를 p1 커서에서 찾고 없다면 새 레코드를 생성
         # 그 후 해당 레코드의 데이터에 정수 테이블 키를 추가하고 p1 커서 파일에 작성
+        
+        # TODO: 여기 로직 다 변경됐음. 확인 필요
         elif pOp.opcode == OP_PutIdx:
-          pass
+          i = pOp.p1
+          tos = self.aStack.pop()
+          nos = self.aStack.pop()
+          pCrsr = self.aCsr[i].pCursor
+
+          if i >= 0 and i < self.nCursor and pCrsr != 0:
+            # 원본 소스코드는 굉장히 복잡해 보이지만 메모리 관리 때문에 케이스가 나뉨
+            # 파이썬에서는 메모리 관리를 신경쓰지 않기 때문에 간소화
+            # HACK: pCrsr에 key=tos, value=nos로 가지는 행 put
+            r = pCrsr.fetch(tos)
+
+            if r == 0:
+              pCrsr.put(tos, nos)
+
+            else:
+              pCrsr.data.append(nos)
+              pCrsr.put(tos, pCrsr.data)
         
         # 스택의 top은 SQL index 키, 그 다음 값은 SQL table entry 키인 정수 값
         # p1 커서에서 tos에 있는 인덱스 키와 일치하는 레코드를 찾고
@@ -918,7 +944,29 @@ class Vdbe:
         # 만약 해당 작업으로 p1 커서의 데이터에서 마지막 정수 테이블 키까지 모두 제거되면
         # p1 커서에서 대응되는 레코드도 삭제
         elif pOp.opcode == OP_DeleteIdx:
-          pass
+          i = pOp.p1
+          tos = self.aStack.pop()
+          nos = self.aStack.pop()
+          pCrsr = self.aCsr[i].pCursor
+
+          if i >= 0 and i < self.nCursor and pCrsr != 0:
+            r = pCrsr.fetch(tos)
+
+            if r == 0:
+              pc += 1
+              continue
+
+            if len(pCrsr.data) == 1 and pCrsr.data[0] == nos:
+              pCrsr.delete(tos)
+
+            else:
+              try:
+                idx = pCrsr.data.index(nos)
+              except:
+                pc += 1
+                continue
+              del pCrsr.data[idx]
+              pCrsr.put(tos, pCrsr.data)
 
         # 파일 이름이 p3 인 파일을 디스크에서 삭제
         elif pOp.opcode == OP_Destroy:
@@ -957,7 +1005,7 @@ class Vdbe:
           i = pOp.p1
           if i < 0 or i > len(self.apList) or self.apList[i] == 0:
             # (TODO) continue 아니고 bad_instruction 오류로 수정해야함
-            continue
+            raise BadInstruction(f"illegal operation at {pc}")
 
           val = self.apList[i].readline()
           if val == '':
@@ -989,7 +1037,7 @@ class Vdbe:
           data = str(self.aStack.pop())
           if i < 0 or i >= len(self.apSort):
             # (TODO) continue 아니고 bad_instruction 오류 발생해야함
-            continue
+            raise BadInstruction(f"illegal operation at {pc}")
           
           pSorter = Sorter()
           pSorter.pNext = self.apSort[i]
@@ -1030,11 +1078,11 @@ class Vdbe:
         elif pOp.opcode == OP_Sort:
           j = pOp.p1
           # Number of buckets used for merge-sort.
-          NSORT = 30  
+          NSORT = 30
           if j <len(self.apSort):
-            apSorter = list(NSORT)
+            apSorter = [0] * NSORT
 
-            while self.apSort[j]:
+            while self.apSort[j] != 0:
               pElem = self.apSort[j]
               self.apSort[j] = pElem.pNext
               pElem.pNext = 0
@@ -1053,16 +1101,20 @@ class Vdbe:
             for i in range(NSORT):
               pElem = Merge(apSorter[i], pElem)
 
-            self.apSort[i] = pElem
+            self.apSort[j] = pElem
 
         # p1 sorter의 topmost 원소의 데이터를 스택에 push 후, sorter에서 원소 삭제
         elif pOp.opcode == OP_SortNext:
           i = pOp.p1
-          if i < 0 or i >= len(self.apSort):
+          if i < 0:
+            raise BadInstruction(f"illegal operation at {pc}")
+          if i < len(self.apSort) and self.apSort[i] != 0:
             pSorter = self.apSort[i]
             self.apSort[i] = pSorter.pNext
             self.aStack.append(pSorter.pData)
             del pSorter
+          else:
+            pc = pOp.p2 - 1
 
         # p1 sorter의 topmost 원소의 key를 스택에 push
         # sorter는 건들지 않음
@@ -1078,6 +1130,9 @@ class Vdbe:
           record = self.aStack.pop()
           if xCallback != None:
             xCallback(pOp.p1, record, [])
+          else:
+            print("### CALLBACK DEBUGGING ###")
+            print(record)
 
         # p1 sorter를 닫고 모든 원소를 삭제
         elif pOp.opcode == OP_SortClose:
@@ -1120,6 +1175,7 @@ class Vdbe:
         elif pOp.opcode == OP_FileRead:
           if self.pFile == 0:
             # (TODO) goto fileread_jump;
+            pc += 1
             continue
 
           nField = pOp.p1
